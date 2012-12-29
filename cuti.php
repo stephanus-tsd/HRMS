7517df1c3f7426f20e7d64864920e2e64291b2b0
stephanus tsd <stephanus_tsd@yahoo.com>
2012-12-29 21:18:40 +0700

    link.php

--- cuti.php
+++ cuti.php
@@ -5,7 +5,7 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
+<title>Pengajuan Cuti</title>
 <script type='Text/JavaScript' src='include/scw.js'></script>
 
 <script type="text/javascript">
--- include/link.php
+++ include/link.php
@@ -1,10 +1,26 @@
 <?php
+session_start();
 ?>
 <div align="center">
-<div>
-	<p><a href="">HOME</a>  |  <a href="">ABSENSI</a>  |  <a href="">CUTI / IJIN</a></p>
+<div id="myjquerymenu" class="jquerycssmenu">
+	<?php
+	if ($_SESSION['user'] == "admin") {
+		echo "<ul>";
+			echo "<li><a href='HOME.php'>Home</a></li>";
+			echo "<li>Employee</li>";
+				echo "<ul>";
+					echo "<li><a href='employee_list.php'>Employee List</a></li>";
+					echo "<li><a href='input_new_employee.php'>Input New Employee</a></li>";
+				echo "</ul>";
+			echo "<li>Cuti</li>";
+				echo "<ul>";
+					echo "<li><a href='cuti.php'>Pengajuan Cuti</a></li>";
+					echo "<li><a href=''>Statistik Pengajuan Cuti</a></li>";
+		echo "</ul>";
+	}
+	?>
 </div>
 <div style="float:right; margin-top:-38px;">
-	<p><a href="" id="login">LOGIN</a>
+	<p><a href="../LOGIN.php" id="login">LOGIN</a>
 </div>
 </div>
