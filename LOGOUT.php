18bda955b6e3cec66e680b8af5731e7dbde08f5a
stephanus tsd <stephanus_tsd@yahoo.com>
2012-12-30 19:31:17 +0700

    logout(clear)

---
+++ LOGOUT.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include "include/library.php";
+
+$nama = $_SESSION['nama'];
+
+unset($_SESSION['nama']);
+unset($_SESSION['jabatan']);
+unset($_SESSION['user']);
+
+echo "Terima kasih saudara $nama atas kunjungannya.<br />";
+echo "<a href='HOME.php'>Klik disini</a> untuk kembali.";
+?>
